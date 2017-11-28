deb5c2ffacfa1e1d8e1273ad5c92d9072be79848
seiruss <seiruss@users.noreply.github.com>
2017-11-28 15:58:30 -0600

    1.0

--- src/check_tags.c
+++ src/check_tags.c
@@ -18,13 +18,19 @@ static void usage(char *argv[])
 
 static void version(char *argv[])
 {
-	printf("%s BETA\n", basename(argv[0]));
+	printf("%s 1.0\n", basename(argv[0]));
 	printf("Written by Russell Seifert\n");
 	printf("Contribute at <https://github.com/seiruss/check-tags>\n\n");
 }
 
 int main(int argc, char *argv[])
 {
+	if (argc != 2)
+	{
+		usage(argv);
+		return EXIT_FAILURE;
+	}
+
 	for (int i = 1; i < argc; i++)
 	{
 		if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0)
@@ -37,12 +43,6 @@ int main(int argc, char *argv[])
 			version(argv);
 			return EXIT_SUCCESS;
 		}
-	}
-
-	if (argc != 2)
-	{
-		usage(argv);
-		return EXIT_FAILURE;
 	}
 
 	FILE *fp = open_file(argv[1]);
